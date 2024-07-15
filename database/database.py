eb3fb8dee31d43c3f1c89039ad4c285d72caf626
Naman Jain <naman.j@consultadd.com>
2024-07-15 08:17:16 -0400

    Completed setup of database for the project.

---
+++ database/database.py
@@ -0,0 +1,31 @@
+from os.path import join as pathjoin
+from pathlib import Path
+from sqlalchemy import create_engine
+from configparser import ConfigParser
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+config = ConfigParser()
+config_file_path = Path(__file__).resolve().parent.parent
+config_file = pathjoin(config_file_path, 'config', 'config.ini')
+config.read(config_file)
+
+
+SQLALCHEMY_DATABASE_URL = config['database']['database_url']
+
+ENGINE = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+SESSION_LOCAL = sessionmaker(autocommit=False, autoflush=False, bind=ENGINE)
+
+BASE = declarative_base()
+
+
+class Database:
+    db = SESSION_LOCAL()
+
+    @classmethod
+    def get_db(cls):
+        try:
+            yield cls.db
+        finally:
+            cls.db.close()
---
+++ database/migrations.py
@@ -0,0 +1,8 @@
+import os
+from pathlib import Path
+from models import models
+from .database import ENGINE
+
+
+def create_database():
+    models.BASE.metadata.create_all(bind=ENGINE)
